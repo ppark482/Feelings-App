57f118dbd76a79d92ae814393103af539ebf9513
rdanieldesign <rdanieldesign@gmail.com>
2014-11-01 19:19:09 -0400

    change server

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,10 +3,11 @@
 // Collection Constructor is Group
 // -------------------------------------------------------------------------------------------------------------------------------------------------
 var findID;
+var uniqueID;
 
 $('#update').on('submit', function(e){
 
-  e.preventDefault();
+  // e.preventDefault();
 
   var secret = $('#secretID').val();
   var newHigh = $('#highUpdate').val();
@@ -18,6 +19,23 @@ $('#update').on('submit', function(e){
   //   console.log(findID);
   //   // entire_group.add(findID);
   // });
+
+  findID = entire_group.findWhere({sID : secret});
+  uniqueID = findID.attributes._id;
+  console.log(uniqueID);
+
+  var matching = entire_group.findWhere({_id: uniqueID});
+  matching.attributes.high = newHigh;
+  matching.save();
+
+  // _.each(entire_group, function(x){
+  //   if(x._id === uniqueID){
+  //     console.log(x);
+  //   }
+  // });
+
+  // console.log(uniqueID);
+  // findID.set('high', newHigh).set('low', newLow);
 
   // uniqueID.set('high', newHigh).set('low', newLow);
   // Save groupArray to server
