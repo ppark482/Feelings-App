21f79be15c084344fdb1426a84dc120d1a06ed47
Paul Park <ppark482@gmail.com>
2014-11-03 13:00:42 -0500

    added views and templating

--- app/scripts/view.js
+++ app/scripts/view.js
@@ -1,10 +1,31 @@
-  initialize: function(){
-      console.log('classmate view initialized');
+var ClassView = Backbone.View.extend({
+
+  tagName: 'ul',
+  className: 'profile',
+
+  initialize: function(options) {
+    this.render(options.collection);
   },
 
+  render: function( collection) {
+    // Templates
+    var gridTemp = $('#grid').html();
+    var renderGrid = _.template(gridTemp);
 
-  render: function(){
+    // Binding self
+    var self = this;
+    // iterating through the entire_group
+    _.each(collection.models, function(user){
+      self.$el.append(renderGrid(user.attributes));
+    });
 
-  }
+    // Take data and append to specific
+    // DOM element
+    $('.profileGrid').html(this.el);
+
+    return this;
+
+  } // end of render
+
 
 });
