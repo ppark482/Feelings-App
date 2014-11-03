0dfa0f47da3209d2150c1f150988564d6ebde55a
Paul Park <ppark482@gmail.com>
2014-11-03 09:09:50 -0500

    view templating

--- app/scripts/view.js
+++ app/scripts/view.js
@@ -2,10 +2,19 @@ var ClassmateView = Backbone.View.extend({
 
   initialize: function(){
       console.log('classmate view initialized');
+  },
+
+  // Compile the template using underscore
+  template: _.template( $("#classmate_template").html(), {} ),
+
+  render: function(){
+      // Load the compiled HTML into the Backbone "el"
+      this.$el.html(this.template);
   }
 
 });
 
 // The initialize function is always called when instantiating a Backbone View.
 // Consider it the constructor of the class.
-var classmate_view = new ClassmateView();
+
+var classmate_view = new ClassmateView({ el: $('.classmates') });
