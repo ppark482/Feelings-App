d05b0d22976656300070031bcad4ca09fb1af6a4
Paul Park <ppark482@gmail.com>
2014-11-03 08:41:49 -0500

    adding backbone views

---
+++ app/scripts/view.js
@@ -0,0 +1,11 @@
+var ClassmateView = Backbone.View.extend({
+
+  initialize: function(){
+      console.log('classmate view initialized');
+  }
+
+});
+
+// The initialize function is always called when instantiating a Backbone View.
+// Consider it the constructor of the class.
+var classmate_view = new ClassmateView();
