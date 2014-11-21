9ac8d15b3c771d057fece73de26abf581b9e56b9
rdanieldesign <rdanieldesign@gmail.com>
2014-11-21 08:34:52 -0500

    Small change

--- app/scripts/feel_factory.js
+++ app/scripts/feel_factory.js
@@ -4,7 +4,7 @@
 
 		.factory('FeelFactory', ['$rootScope', '$http', 'Restangular', function($rootScope, $http, Restangular) {
 
-			var url = 'http://tiy-atl-fe-server.herokuapp.com/collections/feelings6/';
+			var feelsBase = Restangular.all('feelings6'); 
 
 			function getClassmates() {
 				return $http.get(url);
