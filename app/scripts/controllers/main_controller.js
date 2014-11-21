be33a36bee456e5063f607a80b15e2216ad22cdf
rdanieldesign <rdanieldesign@gmail.com>
2014-11-21 10:19:57 -0500

    Revert back to http

--- app/scripts/controllers/main_controller.js
+++ app/scripts/controllers/main_controller.js
@@ -4,10 +4,10 @@
 
 		.controller('MainController', ['FeelFactory', '$scope', '$location', '$rootScope', function(FeelFactory, $scope, $location, $rootScope) {
 
-			FeelFactory.getClassmates().then(function(results){
+			FeelFactory.getClassmates().success(function(results){
 
 				$scope.entireGroup = results;
-				
+
 			});
 
 		}]); // end controller
